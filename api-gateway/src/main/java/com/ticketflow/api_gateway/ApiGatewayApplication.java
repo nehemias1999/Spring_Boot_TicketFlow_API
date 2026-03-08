e2c52ed8eeac75794b9ea98613b22ab3901b732e
Nehemias Salazar <nehemiassala@gmail.com>
2026-03-08 18:27:04 -0300

    Update Java and Spring Cloud versions, fix API Gateway package naming, and adjust project configuration files

--- api-gateway/src/main/java/com/ticketflow/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ticketflow/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ticketflow.api-gateway;
+package com.ticketflow.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
